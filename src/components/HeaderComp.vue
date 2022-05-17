3223855fac1267df8811169957e7edad54abbf8d
IvanRusso91 <iv.russo91@gmail.com>
2022-05-17 16:09:27 +0200

    inserito Header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="container-fluid">
-    ciao
+  <div class="container-fluid d-flex ir-head">
+    <img src="../assets/img/logo-small.svg" alt="">
+
+    <div class="dropdown btn-box">
+      <button class="btn ir-btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        Seleziona un genere
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+</div>
   </div>
 </template>
 
@@ -10,6 +21,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+@import '../assets/style/general';
+@import '../assets/style/vars';
+
+
+.ir-head{
+  height: 80px;
+  justify-content: space-between;
+  background-color: $primary-color;
+  img{
+    transform: scale(0.5);  
+  }
+  
+  .btn-box{
+    margin-top: 20px;
+    margin-right: 15px;
+    .ir-btn{
+      background-color: white;
+    }
+  }
+}
+
+
 
 </style>
